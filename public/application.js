1f871e78cd402f170052c78c8db387524a4f2c78
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2022-07-09 09:37:08 +0200

    remove unneeded code

--- public/application.js
+++ public/application.js
@@ -323,12 +323,3 @@ get_process();
 get_data();
 get_moddate();
 
-var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
-triggerTabList.forEach(function (triggerEl) {
-  var tabTrigger = new bootstrap.Tab(triggerEl)
-
-  triggerEl.addEventListener('click', function (event) {
-    event.preventDefault()
-    tabTrigger.show()
-  })
-})
